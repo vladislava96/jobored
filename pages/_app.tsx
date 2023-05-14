cacbbb677537488136f9d5eadd3db44f7191c0fe
ULADZISLAVA <vladislava96.96@mail.ru>
2023-05-14 17:06:07 +0300

    feat: Create redux store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,10 @@ import NextApp, { AppProps, AppContext } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
+import { Provider as ReduxProvider } from 'react-redux';
 import Header from '../components/Header/Header';
 import './global.css';
+import store from '../app/store';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
@@ -15,11 +17,13 @@ export default function App(props: AppProps) {
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
-      <Header />
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <Component {...pageProps} />
-        <Notifications />
-      </MantineProvider>
+      <ReduxProvider store={store}>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <Header />
+          <Component {...pageProps} />
+          <Notifications />
+        </MantineProvider>
+      </ReduxProvider>
     </>
   );
 }
